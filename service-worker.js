2632f78fd7799d7fa8ca14ab65eddb510404593c
ARCARVALHO1000 <arcinfoprodutos@gmail.com>
2026-09-26 08:32:55 -0300

    Update cache name to version 4-idiomas-v4-4b

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = '4-idiomas-v4-3a';
+const CACHE_NAME = '4-idiomas-v4-4b';
 const APP_FILES = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', event => {
